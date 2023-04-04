dbaab6471c1bc60843fbe88667e33b0247b627fd
196code-gray <doco827@gmail.com>
2023-04-04 23:18:41 +0900

    two

--- src/main/java/com/codestates/practicegithubactiondeploy/controller/HelloController.java
+++ src/main/java/com/codestates/practicegithubactiondeploy/controller/HelloController.java
@@ -1,9 +1,10 @@
 package com.codestates.practicegithubactiondeploy.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/")
     public String hello () {
